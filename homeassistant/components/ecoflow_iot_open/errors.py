8635c6eba26056a07ebd4d89c738dd1110681f3f
Nid01 <30300755+Nid01@users.noreply.github.com>
2025-11-22 21:24:44 +0000

    feat(ecoflow): add DeviceNotAllowedError for blocked device requests

--- homeassistant/components/ecoflow_iot_open/errors.py
+++ homeassistant/components/ecoflow_iot_open/errors.py
@@ -11,6 +11,14 @@ class CannotConnect(EcoFlowIoTOpenError):
     """Error to indicate we cannot connect."""
 
 
+class DeviceNotAllowedError(EcoFlowIoTOpenError):
+    """Response code 1006 indicates, that EcoFlow decided to block HTTP requests for the chosen device.
+
+    Nesserary since EcoFlow decided to lock out devices which aren't officially supported from the iot "open" API (yet?).
+    https://www.reddit.com/r/Ecoflow_community/comments/1lzp7qa/comment/n3e0tkn/
+    """
+
+
 class InvalidAuth(EcoFlowIoTOpenError):
     """Error to indicate there is invalid auth."""
 
